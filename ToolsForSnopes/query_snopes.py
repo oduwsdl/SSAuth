32cb90f1fc64c1959b7b0f48d7ad4172ef16cd16
calebkbrad <92956463+calebkbrad@users.noreply.github.com>
2022-07-13 14:10:46 -0400

    Added filter for only fact-checks

--- ToolsForSnopes/query_snopes.py
+++ ToolsForSnopes/query_snopes.py
@@ -3,7 +3,14 @@ import sys
 from bs4 import SoupStrainer as ss
 import requests
 from urllib.parse import urlencode
+import re
 
+# Function to generate encoded query from a string
+def generate_query_url(snopes_query):
+    query_dict = {'s': snopes_query}
+    return ('?' + urlencode(query_dict))
+
+# Function to 
 def grab_links(query_url):
     # Setup bs object
     only_links = ss('a', class_="link")
@@ -18,31 +25,32 @@ def grab_links(query_url):
 
 def grab_rating(article_url):
     print('grabbing rating')
-    rating_strainer = ss(class_ = 'figure-image img-responsive img-fluid w-100 Media--image')
+    rating_strainer = ss(class_ = ['figure-image img-responsive img-fluid w-100 Media--image', 'figure-image img-responsive img-fluid w-100'])
     html = requests.get(article_url)
     html_text = html.text
     soup = bs(html_text, 'html.parser', parse_only = rating_strainer)
+    print(soup.prettify())
     tag = soup.img
-    return tag.get('alt')
 
-def generate_query_url(snopes_query):
-    query_dict = {'s': snopes_query}
-    print(urlencode(query_dict))
-    return ('?' + urlencode(query_dict))
+    return tag.get('alt')
 
 def main(argv):
     print('I have started')
-    query_str = 'I guess we ran out of microchips'
+    query_str = 'trump tweet'
     snopes_url = "https://www.snopes.com/"
     query = snopes_url + generate_query_url(query_str)
     print(query)
 
     list_of_links = grab_links(query)
 
-    print(list_of_links)
+    fact_check_url = 'https://www.snopes.com/fact-check/'
 
-    article_url = list_of_links[0]
-    print(grab_rating(article_url))
+    list_of_links = [link for link in list_of_links if link[:34] == fact_check_url]
+
+    print(list_of_links)
+    for link in list_of_links:
+        print(link)
+        print(grab_rating(link))
 
 
 
