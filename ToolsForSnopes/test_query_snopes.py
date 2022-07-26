318e64ac74ed4097e33265bd8109eb78727b8604
calebkbrad <92956463+calebkbrad@users.noreply.github.com>
2022-07-26 16:25:14 -0400

    Fixed Snopes test?

--- ToolsForSnopes/test_query_snopes.py
+++ ToolsForSnopes/test_query_snopes.py
@@ -6,7 +6,7 @@ def test_grab_links():
     tweet = "There is nothing that I would want more for our Country than true FREEDOM OF THE PRESS. The fact is that the Press is FREE to write and say anything it wants, but much of what it says is FAKE NEWS, pushing a political agenda or just plain trying to hurt people. HONESTY WINS!"
     query = generate_snopes_query(tweet)
     list_of_links = grab_links(query)
-    assert(len(list_of_links) == 0)
+    assert(len(list_of_links) == 1)
     assert(list_of_links[0] == "https://www.snopes.com/fact-check/trump-tweet-echo-mein-kampf/")
 
 def test_grab_rating():
